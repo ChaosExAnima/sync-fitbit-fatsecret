01aaf51035b2d530c9581f067ace8414ca844556
Echo G <ChaosExAnima@users.noreply.github.com>
2020-05-24 21:43:58 -0400

    Provides way to get server headers.

--- trait-request.php
+++ trait-request.php
@@ -3,18 +3,19 @@
 trait Request {
 	private array $response_cache = [];
 
-	private function base_request( string $path, string $method = 'GET', ?array $args = null, bool $parse = true ) {
+	private function base_request(
+		string $path,
+		string $method = 'GET',
+		?array $args = null,
+		bool $parse = true,
+		bool $return_headers = false
+	) {
 		if ( ! empty( $args['params'] ) ) {
 			$path .= '?' . http_build_query( $args['params'] );
 		}
 
-		$headers = [];
-		if ( ! empty( $args['headers'] ) ) {
-			$headers = array_merge( $headers, $args['headers'] );
-		}
-
 		$flat_headers = [];
-		foreach ( $headers as $name => $value ) {
+		foreach ( $args['headers'] ?? [] as $name => $value ) {
 			$flat_headers[] = "$name: $value";
 		}
 
@@ -30,23 +31,43 @@ trait Request {
 		}
 
 		curl_setopt( $ch, CURLOPT_HTTPHEADER, $flat_headers );
-
 		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
+
+		if ( $return_headers ) {
+			$headers = [];
+			curl_setopt( $ch, CURLOPT_HEADERFUNCTION, function( $curl, $header ) use ( &$headers ) {
+				$len = strlen( $header );
+				$header = explode( ':', $header, 2 );
+				if ( count( $header ) < 2 ) {
+					return $len;
+				}
+				$headers[ trim( $header[0] ) ] = trim( $header[1] );
+
+				return $len;
+			} );
+		}
+
 		$response = curl_exec( $ch );
 		curl_close( $ch );
 
 		if ( ! $parse ) {
+			if ( $return_headers ) {
+				return compact( 'response', 'headers' );
+			}
 			return $response;
 		}
 
-		$parsed_response = json_decode( $response );
+		$response = json_decode( $response );
 
-		if ( null === $parsed_response ) {
+		if ( null === $response ) {
 			throw new Exception( 'Got unparseable response.' );
 		}
 
-		$this->response_cache[ $cache_key ] = $parsed_response;
+		$this->response_cache[ $cache_key ] = $response;
 
-		return $parsed_response;
+		if ( $return_headers ) {
+			return compact( 'response', 'headers' );
+		}
+		return $response;
 	}
 }
