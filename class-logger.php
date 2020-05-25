b4516a6b0842cd4d80d245c9434601051033c3cc
Echo G <ChaosExAnima@users.noreply.github.com>
2020-05-25 16:13:46 -0400

    Add error handling to logger.

--- class-logger.php
+++ class-logger.php
@@ -2,13 +2,23 @@
 
 class Logger {
 	private DateTimeZone $tz;
+	private bool $trace;
 
-	public function __construct( DateTimeZone $tz = null ) {
-		$this->tz = $tz;
+	public function __construct( DateTimeZone $tz = null, bool $trace = false ) {
+		$this->tz    = $tz;
+		$this->trace = $trace;
 	}
 
-	public function log( string $message ) {
+	public function log( string $message ) : void {
 		echo $this->add_timestamp( "$message\n" );
+	}
+
+	public function error( Throwable $error ) : void {
+		if ( ! $this->trace ) {
+			$this->log( 'ERROR: ' . $error->getMessage() );
+		} else {
+			$this->log( 'ERROR: ' . $error );
+		}
 	}
 
 	private function add_timestamp( string $message ) : string {
