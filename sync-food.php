5b1c4e4e6917334ba33b1870fdb22cf1281a62a3
Echo G <ChaosExAnima@users.noreply.github.com>
2020-05-25 15:01:18 -0400

    Exits sync early if no diary entries.

--- sync-food.php
+++ sync-food.php
@@ -16,11 +16,16 @@ $fatsecret = new FatSecret( $secrets );
 
 $secrets->save();
 
-
-$food_units   = $fitbit->get_food_units();
-$food_entries = $fatsecret->get_food_for_day( $date );
-
 echo 'Syncing food diary for ' . $date->format( 'Y-m-d' ) . ":\n";
+
+$food_entries = $fatsecret->get_food_for_day( $date );
+if ( ! count( $food_entries ) ) {
+	echo "No entries today, exiting.\n";
+	return;
+}
+
+$food_units = $fitbit->get_food_units();
+
 $fitbit->delete_food_entries_for_date( $date );
 echo "Deleted old data.\n";
 foreach ( $food_entries as $food_entry ) {
