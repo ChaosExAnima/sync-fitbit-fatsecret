37541554ea723a1b4801cd208a62e8d1614478b5
Echo G <ChaosExAnima@users.noreply.github.com>
2020-05-25 16:21:40 -0400

    Refactors weight sync script.

--- sync-weight.php
+++ sync-weight.php
@@ -1,55 +1,52 @@
 <?php
 
-require_once 'class-secrets.php';
+require_once 'class-logger.php';
 require_once 'class-health-check.php';
+require_once 'class-secrets.php';
 require_once 'class-fitbit.php';
 require_once 'class-fatsecret.php';
+require_once 'functions.php';
 
-echo "Updating Fat Secret from FitBit...\n";
-
-$script_args = getopt( '', [ 'date_start::', 'date_end::', 'date_tz::', 'check' ] );
+$script_args = getopt( '', [ 'date_start::', 'date_end::', 'date_tz::' ] );
 $date_tz     = new DateTimeZone( $script_args['date_tz'] ?? 'America/New_York' );
 $start_date  = new DateTimeImmutable( $script_args['date_start'] ?? 'today', $date_tz );
 $end_date    = new DateTimeImmutable( $script_args['date_end'] ?? 'today', $date_tz );
+$logger      = new Logger( $date_tz, get_flag_set( 'debug' ) );
+$check       = new HealthCheck();
 
-$secrets   = new Secrets( __DIR__ . '/secrets.json' );
-$fitbit    = new Fitbit( $secrets );
-$fatsecret = new FatSecret( $secrets );
-$check     = new HealthCheck();
+$logger->log( 'Syncing weight between ' . $start_date->format( 'Y-m-d' ) . ' and ' . $end_date->format( 'Y-m-d' ) . ':' );
 
-if ( false === ( $script_args['check'] ?? null ) ) {
-	$check->init( $secrets );
-}
+try {
+	$secrets   = new Secrets( __DIR__ . '/secrets.json' );
+	$fitbit    = new Fitbit( $secrets );
+	$fatsecret = new FatSecret( $secrets );
 
-$check->start();
-$secrets->save();
+	if ( get_flag_set( 'check' ) ) {
+		$check->init( $secrets );
+	}
 
-$updated      = 0;
-$current_date = DateTime::createFromImmutable( $start_date );
-do {
-	try {
+	$check->start();
+	$secrets->save();
+
+	$updated      = 0;
+	$current_date = DateTime::createFromImmutable( $start_date );
+	do {
 		$weights = $fitbit->get_weight_for_date_period( $current_date, '1m' );
-	} catch ( Exception $err ) {
-		$check->fail();
-		throw $err;
-	}
-	$current_date->add( new DateInterval( 'P1M' ) );
-	foreach ( $weights as $date => $kg ) {
-		$current_day = new DateTimeImmutable( $date, $date_tz );
-		if ( $start_date > $current_day ) {
-			continue;
-		}
-		try {
+		$current_date->add( new DateInterval( 'P1M' ) );
+		foreach ( $weights as $date => $kg ) {
+			$current_day = new DateTimeImmutable( $date, $date_tz );
+			if ( $start_date > $current_day ) {
+				continue;
+			}
 			$fatsecret->update_weight_for_date( $current_day, $kg );
-		} catch ( Exception $err ) {
-			$check->fail();
-			throw $err;
+			$updated++;
+			$logger->log( "Updated weight on {$date}." );
 		}
-		$updated++;
-		echo "Updated weight on {$date}.\n";
-	}
-} while ( $current_date < $end_date );
+	} while ( $current_date < $end_date );
 
-$check->finish();
-
-echo "Updated {$updated} days.\n";
+	$check->finish();
+	$logger->log( "Updated {$updated} days." );
+} catch ( Exception $err ) {
+	$check->fail();
+	$logger->error( $err );
+}
